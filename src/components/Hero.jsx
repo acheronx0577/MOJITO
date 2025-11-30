53dbc788ccad9f137b925efea0f89392a42fc693
AcheronX. <richardhuynh53@gmail.com>
2025-11-30 14:29:08 -0800

    Implement GSAP animation for Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,32 @@
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+import gsap from 'gsap';
+
 const Hero = () => {
-	return (
-		<>
+    useGSAP(() => {
+        const heroSplit = new SplitText('.title', { type: 'chars, words' });
+        const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
+
+        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
+        gsap.from(heroSplit.chars, {
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06
+        });
+
+        gsap.from(paragraphSplit.lines, {
+            opacity: 0,
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
+            delay: 1,
+        })
+    }, []);
+
+    return (
+        <>
             <section id="hero" className="noisy">
                 <h1 className="title">MOJITO</h1>
 
@@ -25,10 +51,7 @@ const Hero = () => {
 
                         <div className="view-cocktails">
                             <p className="subtitle">
-                                Every cocktail on our menu is a
-                                blend of premium ingredients,
-                                creative flair, and timeless recipes
-                                — designed to delight your senses.
+                                Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes — designed to delight your senses.
                             </p>
                             <a href="#cocktails">View Cocktails</a>
                         </div>
