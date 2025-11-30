2c5374269c6b7e9e465289b67ee063049a7e4b30
AcheronX. <richardhuynh53@gmail.com>
2025-11-30 13:44:13 -0800

    feat: Add Navbar component and integrate GSAP for dynamic background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,30 @@
+import { useGSAP } from "@gsap/react";
 import { navLinks } from "../../constants";
+import gsap from 'gsap';
 
 const Navbar = () => {
+	useGSAP(() => {
+		const navTween = gsap.timeline({
+			ScrollTrigger: {
+				trigger: 'nav',
+				start: 'bottom top'
+			}
+		});
+
+		navTween.fromTo('nav', { backgroundColor: 'transparent'}, {
+			backgroundColor: '#00000050',
+			backgroundFilter: 'blur(10px)',
+			duration: 1,
+			ease: 'power1.in0ut'
+		});
+	})
+
+
 	return (
 		<nav>
 			<div>
 				<a href="#home" className="flex items-center gap-2">
+					<img src="/images/logo.png" alt="logo" />
 					<p>Velvet Pour</p>
 				</a>
 				<ul className="flex gap-4 mt-4">
